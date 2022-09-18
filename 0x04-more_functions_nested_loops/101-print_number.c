2ec5587e7c13d17b9aa1bbb31388499c50df57fd
ibrozy10 <olajideibrahimabidemi@gmail.com>
2022-09-18 15:39:41 -0700

    Print Number

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -2,9 +2,9 @@
 #include "main.h"
 
 /**
- *  print_number - prints an integer
- *  @n: random integer
- *  Return: void
+ * print_number - prints an integer
+ * @n: random integer
+ *Return: void
  */
 
 void print_number(int n)
