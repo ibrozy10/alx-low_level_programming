db17c204dc4a4809b833f34047947f06aeac60eb
ibrozy10 <olajideibrahimabidemi@gmail.com>
2022-09-22 10:32:55 -0700

    Print Number C

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -20,12 +20,11 @@ void print_number(int n)
 		n = n / -10;
 	}
 
-       	else
+                else
 	{
 		ld = (char)((n % 10) + '0');
 		n = n / 10;
-	}
-	
+       	}	
 
 	r = 0;
 	while (n > 0)
