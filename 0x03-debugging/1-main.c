9067001841b97f8d33f45a80e30589cc9ab9deb1
ibrozy10 <olajideibrahimabidemi@gmail.com>
2022-09-22 04:40:49 -0700

    Main

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -13,10 +13,10 @@ int main(void)
 	i =
 
 		/**
-		 *while (i < 10)
-		 *{
-		 *putchar(i);
-		 *}
+		 * while (i < 10)
+		 * {
+		 * putchar(i);
+		 * }
 		 */
 
 		printf("Infinite loop avoided! \\o/\n");
