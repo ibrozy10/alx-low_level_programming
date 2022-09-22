6164934c4f4cdf1ddb5c88dc12833d9e7b699242
ibrozy10 <olajideibrahimabidemi@gmail.com>
2022-09-22 15:08:50 -0700

    Print Tebahpla

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -9,10 +9,10 @@
 int main(void)
 {
 	char c;
-	for (c = 'z'; c >= 'a'; c--)
-	{
-		putchar(c);
-	}
-	putchar('\n');
-	return (0);
+for (c = 'z'; c >= 'a'; c--)
+{
+	putchar(c);
+}
+putchar('\n');
+return (0);
 }
